3a446d8d6f3bdef20714a2191ef3c8a2bc5c4be9
hwipl <33433250+hwipl@users.noreply.github.com>
2021-10-05 20:17:10 +0200

    tview/hello: change left side to list
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- tview/hello/hello.go
+++ tview/hello/hello.go
@@ -9,8 +9,15 @@ import (
 
 func main() {
 	app := tview.NewApplication()
-	left := tview.NewBox().
-		SetBorder(true).
+	left := tview.NewList().
+		AddItem("Hello 1", "Hello", 'a', nil).
+		AddItem("Hello 2", "Hello", 'b', nil).
+		AddItem("Hello 3", "Hello", 'c', nil).
+		AddItem("Hello 4", "Hello", 'd', nil).
+		AddItem("Bye", "Bye", 'q', func() {
+			app.Stop()
+		})
+	left.SetBorder(true).
 		SetBorderColor(tcell.ColorBlue).
 		SetBorderAttributes(tcell.AttrBold).
 		SetTitle("[::b] Hello Left ").
@@ -28,7 +35,7 @@ func main() {
 		SetTitle("[::b] Hello Right Bottom ").
 		SetTitleColor(tcell.ColorBlue)
 	flex := tview.NewFlex().
-		AddItem(left, 0, 1, false).
+		AddItem(left, 0, 1, true).
 		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
 			AddItem(right_top, 0, 3, false).
 			AddItem(right_bottom, 0, 1, false), 0, 2, false)
