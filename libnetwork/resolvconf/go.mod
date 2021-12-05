f8edda56ad1fcee7f46e245bab88e04f97aad05c
hwipl <33433250+hwipl@users.noreply.github.com>
2021-12-05 12:07:35 +0100

    add libnetwork/resolvconf
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ libnetwork/resolvconf/go.mod
@@ -0,0 +1,20 @@
+module github.com/hwipl/snippets-go/libnetwork/resolvconf
+
+go 1.17
+
+require github.com/moby/libnetwork v0.5.6
+
+require (
+	github.com/Sirupsen/logrus v1.0.6 // indirect
+	github.com/docker/docker v20.10.11+incompatible // indirect
+	github.com/docker/libnetwork v0.5.6 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/vishvananda/netns v0.0.0-20211101163701-50045581ed74 // indirect
+	golang.org/x/crypto v0.0.0-20211202192323-5770296d904e // indirect
+	golang.org/x/sys v0.0.0-20211204120058-94396e421777 // indirect
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1 // indirect
+	gopkg.in/airbrake/gobrake.v2 v2.0.9 // indirect
+	gopkg.in/gemnasium/logrus-airbrake-hook.v2 v2.1.2 // indirect
+	gotest.tools/v3 v3.0.3 // indirect
+)
---
+++ libnetwork/resolvconf/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+
+	"github.com/moby/libnetwork/resolvconf"
+)
+
+func main() {
+	// get default resolv.conf
+	r, err := resolvconf.Get()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get nameservers
+	for _, ns := range resolvconf.GetNameservers(r.Content) {
+		log.Println("Nameserver:", ns)
+	}
+
+	// get nameservers in CIDR notation
+	for _, ns := range resolvconf.GetNameserversAsCIDR(r.Content) {
+		log.Println("Nameserver (CIDR):", ns)
+	}
+
+	// get options
+	for _, op := range resolvconf.GetOptions(r.Content) {
+		log.Println("Option:", op)
+	}
+
+	// get search domains
+	for _, sd := range resolvconf.GetSearchDomains(r.Content) {
+		log.Println("Search Domain:", sd)
+	}
+
+	// get specific resolv.conf
+	s, err := resolvconf.GetSpecific("/etc/resolv.conf")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if s.Hash != r.Hash {
+		log.Println("resolv.confs are not equal")
+	} else {
+		log.Println("resolv.confs are equal")
+	}
+}
