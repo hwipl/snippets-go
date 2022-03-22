bd8dfe2d0a389fdf0f16af1cf2ecba687de9f1e1
hwipl <33433250+hwipl@users.noreply.github.com>
2022-03-22 23:04:40 +0100

    logrus/log: add more examples
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- logrus/log/main.go
+++ logrus/log/main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"errors"
+
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	logrus.SetLevel(logrus.TraceLevel)
+	// Println adds spaces, other functions do not
+	logrus.Println("Hello,", "Info,", "from", "Println")
+
 	logrus.Trace("Hello, Trace")
-	logrus.Debug("Hello, Debug")
+	logrus.Debug("Hello, ", "Debug")
 	logrus.Info("Hello, Info")
 	logrus.Warn("Hello, Warn")
-	logrus.Error("Hello, Error")
+	logrus.Error("Hello, ", errors.New("Error"))
 	// logrus.Fatal("Bye, Fatal")
 	// logrus.Panic("Bye, Panic")
 	logrus.WithFields(logrus.Fields{
 		"from": "Logrus",
 		"to":   "World",
 	}).Info("Hello")
+	logrus.WithError(errors.New("Something went wrong!")).Error("Oops")
 	logrus.WithFields(logrus.Fields{
 		"from": "Logrus",
 		"to":   "World",
