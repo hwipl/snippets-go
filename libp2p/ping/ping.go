18e219ef645d319c5e920393bd399848041a51b8
hwipl <33433250+hwipl@users.noreply.github.com>
2023-02-26 11:22:13 +0100

    libp2p/ping: update dependencies
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- libp2p/ping/ping.go
+++ libp2p/ping/ping.go
@@ -17,10 +17,10 @@ import (
 	"syscall"
 
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/network"
-	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/go-libp2p-core/peerstore"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/peerstore"
 	multiaddr "github.com/multiformats/go-multiaddr"
 )
 
